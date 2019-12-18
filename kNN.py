2d978445156689b7c18f70d712cb90156520a47f
Yin169 <yincheang.ng@outlook.com>
2019-12-18 18:02:07 +0800

    fix bug

--- kNN.py
+++ kNN.py
@@ -1,30 +1,34 @@
+import heapq
 import numpy as np
 import matplotlib.pyplot as plt
 
 class Node(object):
     def __init__(self, axis=None, val=[], left=None, right=None):
         self.axis = axis
-        self.val = val
+        self.point = val
         self.left = left
         self.right = right
 
 class KDtree(object):
-    def __init__(self, point):
+    def __init__(self, point, k):
         self.dim = len(point[0])
-        self.Kbest = None
-        root = self.build_tree(point)
+        self.k = k
+        self.Kbest = []
+        self.root = self.fit(point)
 
     def fit(self, point, depth=0):
-        if point == []:
+        if len(point) <= 1:
             return None
         axis = depth % self.dim
         array = [p[axis] for p in point]
         median = self.find_median(0, len(array)-1, array, len(array)//2)
         index = array.index(median)
+        # left = [i for i in point if i[axis] < median]
+        # right = [i for i in point if i[axis] > median]
         return Node(axis = axis,
                     val = point[index],
-                    left = self.fit(point[:median], deptj=depth+1),
-                    right = self.fit(point[median:], depth=depth+1))
+                    left = self.fit(point=point[:index], depth=depth+1),
+                    right = self.fit(point=point[index:], depth=depth+1))
 
     def find_median(self, l, r, a, index):
         i,j = l,r
@@ -39,14 +43,15 @@ class KDtree(object):
                 a[i],a[j] = a[j],a[i]
                 i +=1
                 j -=1
-        if l<j and index < j:
+        if l<j and l < index < j:
             self.find_median(l, j, a, index)
-        elif i<r and i < index:
+        elif i<r and i < index < r:
             self.find_median(i, r, a, index)
         return a[index]
 
     def find_Knearest(self, point, root=None):
         if root is None:
+            self.Kbest = []
             root = self.root
 
         if root.left is not None or root.right is not None:
@@ -56,16 +61,24 @@ class KDtree(object):
                 self.find_Knearest(point, root=root.right)
 
         dis = sum(list(map(lambda x,y : abs(x-y), root.point, point)))
-        if self.Kbest is None or dis < self.Kbest[0]:
-            self.Kbest = (dis, root.point)
+        if len(self.Kbest) < self.k:
+            heapq.heappush(self.Kbest,(dis, root.point))
+        # print(self.Kbest)
+        # print(heapq.nlargest(1, self.Kbest, lambda d:d[0])[0][0])
+        # print()
+        if dis < heapq.nlargest(1, self.Kbest, lambda d:d[0])[0][0]:
+            self.Kbest.pop(-1)
+            heapq.heappush(self.Kbest, (dis, root.point))
 
-        if abs(root.point[root.axis]-point[root.axis]) < self.Kbest[0]:
+        # if len(self.Kbest) == 0 or dis < self.Kbest[0]:
+        #     self.Kbest = (dis, point)
+
+        if abs(root.point[root.axis]-point[root.axis]) < heapq.nlargest(1, self.Kbest, lambda d:d[0])[0][0]:
             if root.right is not None and point[root.axis] < root.point[root.axis]:
                 self.find_Knearest(point, root=root.right)
             elif root.left is not None and point[root.axis] >= root.point[root.axis]:
                 self.find_Knearest(point, root=root.left)
-
-        return 0
+        return self.Kbest
 
 
 def gen_data(x1, x2):
@@ -74,19 +87,31 @@ def gen_data(x1, x2):
 
 
 def load_data():
-    x1_train = np.linspace(0, 50, 500)
-    x2_train = np.linspace(-10, 10, 500)
-    data_train = np.array(
-        [[x1, x2, gen_data(x1, x2) + np.random.random(1) - 0.5] for x1, x2 in zip(x1_train, x2_train)])
+    x1_train = np.linspace(0, 50, 50)
+    x2_train = np.linspace(-10, 10, 50)
+    data_train = [[x1, x2, gen_data(x1, x2) + np.random.random(1)[0] - 0.5] for x1, x2 in zip(x1_train, x2_train)]
     x1_test = np.linspace(0, 50, 100) + np.random.random(100) * 0.5
     x2_test = np.linspace(-10, 10, 100) + 0.02 * np.random.random(100)
-    data_test = np.array([[x1, x2, gen_data(x1, x2)] for x1, x2 in zip(x1_test, x2_test)])
-    return data_train, data_test
+    data_test = [[x1, x2, gen_data(x1, x2)] for x1, x2 in zip(x1_test, x2_test)]
+    return np.array(data_train), np.array(data_test)
+
 def main():
     train, test = load_data()
-    x_train, y_train = train[:, :2], train[:, 2]
-    x_test, y_test = test[:, :2], test[:, 2]  # 同上，但这里的y没有噪声
-    t = KDtree(x_train)
+    x_train, y_train = train[:, :], train[:, 2]
+    # x_test, y_test = test[:, :2], test[:, 2]  # 同上，但这里的y没有噪声
+    t = KDtree(x_train, k=3)
+    # for i in x_train:
+    #     print(t.find_Knearest(i)[-1])
+    #     print()
+    result = [t.find_Knearest(i)[-1][-1] for i in x_train]
+    result = [i[-1] for i in result]
+    result = [np.average(i) for i in result]
+    #
+    print(result)
+    plt.plot(result)
+    plt.plot(y_train)
+    plt.show()
+
 
     return 0
 if __name__ == "__main__":
